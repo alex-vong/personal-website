12bfda92462e915fb15e1b7d2e76577ea114cec9
Alex Vong <avong003@gmail.com>
2021-11-27 02:44:01 -0800

    added animated line to site-menu

--- masthead.php
+++ masthead.php
@@ -7,10 +7,10 @@
 			</a>
 
 			<nav class="nav-bar">
-				<a href="?page=home">Home</a>
-				<a href="?page=about">About</a>	
-				<a href="?page=projects">Projects</a>
-				<a href="?page=writing">Writing</a>	
+				<a href="?page=home" class="animated-line"><span>Home</span></a>
+				<a href="?page=about" class="animated-line"><span>About</span></a>	
+				<a href="?page=projects" class="animated-line"><span>Projects</span></a>
+				<a href="?page=writing" class="animated-line"><span>Writing</span></a>	
 			</nav>
 		</masthead>
 		
