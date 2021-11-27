b4fc6b295ed7403e53fa5817eeca8f8eed5bb7d9
Alex Vong <avong003@gmail.com>
2021-11-27 02:44:39 -0800

    added whats hot to home page

--- home.php
+++ home.php
@@ -5,15 +5,88 @@
 			<h2 class="section-heading top-level-heading main-heading">Hi, I'm Alex</h2>
 
 			<p class="body-copy intro">
-				I am a <span>web developer</span> passionate about creating  online experiences through <span>Design</span> and <span>Development</span>. This is my site where I share all the projects I've worked on, past and present, as well as blogs talking about all things code and all things not code. 
+				I am a <span>web developer</span> passionate about creating  online experiences through <span>Design</span> and <span>Development</span>.
 			</p>
 
-			<p class="body-copy">
+			<p class="body-copy intro">
+				 This is my site where I share all the projects I've worked on, past and present, as well as blogs talking about all things code. 
+			</p>
+
+
+
+			<p class="body-copy intro">
 				Currently seeking employment as a <span>Front-End Web Developer</span>
 			</p>
 
+			<?php include("modules/contact-me.php") ?> 
+
+
 		</div>
-		<?php include("modules/contact-me.php") ?> 
+
+
+		<div class="whats-hot">
+			<h3 class="section-heading second-level-heading">
+				What's Hot Now
+			</h3>
+
+			<p class="body-copy wh-intro">
+				I enjoy staying busy and working on new projects as well as continue to build and expand projects I have worked in the past. Here's what I am currently working on:
+			</p>
+
+			<h4 class="third-level-heading card-heading">
+				In Projects
+			</h4>
+			<projects-grid>
+
+
+				<?php include("modules/projects-data.php") ?>
+
+
+				<?php foreach ($projects as $project) {
+
+					if($project["hot"]) { ?>
+
+						<project-card>
+							<a href="<?=$project["project-link"]?>">
+								<picture>
+									<img src="<?=$project["image"]?>" alt="">
+								</picture>
+							</a>
+
+							<a href="<?=$project["project-link"]?>">
+								<h4 class="third-level-heading animated-line">
+									<span>
+										<?=$project["name"]?>
+									</span>
+								</h4>
+							</a>
+
+							<p class="body-copy"><?=$project["description"]?></p>
+
+							<div class="card-links">
+								<a href="<?=$project["project-link"]?>" class="animated-line">
+									<span>View Project</span>
+								</a>
+
+								<div class="slash">/</div>
+
+								<a href="<?=$project["case-study"]?>" class="animated-line">
+									<span>View Case Study</span>
+								</a>
+
+							</div>
+							
+						</project-card>
+
+				<?php }
+
+				}
+
+
+				?>
+
+			</projects-grid>
+		</div>
 
 	</inner-column>
 </section>
--- modules/contact-me.php
+++ modules/contact-me.php
@@ -1,7 +1,14 @@
 <section class="contact-me">
+
+
+	<div class="outer-container">
+		<picture>
+			<img src="images/mail.png" alt="">
+		</picture>
+	</div>
 	
-	<h3 class="second-level-heading">
-		Let's Connect!
+	<h3 class="intro-voice">
+		Let's build things <span>together</span>!
 	</h3>
 	<p class="body-copy">
 		You can find me on <a href="mailto:avong003@gmail.com">email</a>, <a href="#">GitHub</a>, <a href="#">Twitter</a>, <a href="#">Instagram</a>
