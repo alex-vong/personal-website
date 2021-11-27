5e933fc46c8f91bc774f1b6a6bbc3ff49b2efc09
Alex Vong <avong003@gmail.com>
2021-11-26 18:13:50 -0800

    created hom php

---
+++ home.php
@@ -0,0 +1,20 @@
+<section class="home">
+	<inner-column>
+		<div class="landing container">
+
+			<h2 class="section-heading top-level-heading main-heading">Hi, I'm Alex</h2>
+
+			<p class="body-copy intro">
+				I am a <span>web developer</span> passionate about creating  online experiences through <span>Design</span> and <span>Development</span>. This is my site where I share all the projects I've worked on, past and present, as well as blogs talking about all things code and all things not code. 
+			</p>
+
+			<p class="body-copy">
+				Currently seeking employment as a <span>Front-End Web Developer</span>
+			</p>
+
+		</div>
+		<?php include("modules/contact-me.php") ?> 
+
+	</inner-column>
+</section>
+</section>
