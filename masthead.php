e6e6f4baaf58014b551afaec10988ac14f7c969b
Alex Vong <avong003@gmail.com>
2021-11-26 18:13:39 -0800

    created masthead php file

---
+++ masthead.php
@@ -0,0 +1,18 @@
+
+	<inner-column>
+		<masthead>
+			<a href="?page=home">
+				<h1 class="intro-voice desktop">Alex On-Line</h1>
+				<h1 class="intro-voice mobile">AOL</h1>
+			</a>
+
+			<nav class="nav-bar">
+				<a href="?page=home">Home</a>
+				<a href="?page=about">About</a>	
+				<a href="?page=projects">Projects</a>
+				<a href="?page=writing">Writing</a>	
+			</nav>
+		</masthead>
+		
+	</inner-column>
+
