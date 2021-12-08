6ed97c590166e89f8450c326225c9950f2c808d5
Alex Vong <avong003@gmail.com>
2021-12-07 23:09:10 -0800

    combined all blog post in project data php object

--- modules/projects-data.php
+++ modules/projects-data.php
@@ -1,39 +1,55 @@
 <?php
 
 $projects = [
+
 	[
-		"name" => "30-Day Responsive Layout Challenge",
+		"name" => "Title of Project",
+		"description" => "Description of the project, what it is and what was the goal",
+		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"case-study" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"image" => "images/project-sg.svg",
+		"hot" => false,
+		"demo" => true,
+	],
+
+	[
+		"name" => "Responsive Layouts Collection",
 		"description" => "A Collection of 30 Responsive Layout Components found in the Wild",
 		"project-link" => "https://peprojects.dev/alpha-1/alexvong/30day-responsive-layout-challenge/",
 		"case-study" => "https://peprojects.dev/alpha-1/alexvong/30day-responsive-layout-challenge/",
-		"image" => "images/rlc-image.svg",
+		"image" => "images/rlc-2.svg",
 		"hot" => true,
+		"demo" => false,
 	],
 	[
 		"name" => "PokeDex",
 		"description" => "A responsive Pokedex created using HTML, CSS, PHP, and PHP Routing.",
 		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
 		"case-study" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
-		"image" => "images/pokedex-image.svg",
+		"image" => "images/pokedex-2.svg",
 
 		"hot" => true,
-	],
-	[
-		"name" => "Student Grade Table",
-		"description" => "Created a CRUD system database where users can create, read, update, and delete student names, classes, and grades in HTML, CSS, PHP, and Javascript.",
-		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
-		"case-study" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
-		"image" => "https://peprojects.dev/images/square.jpg",
-		"hot" => false,
+		"demo" => false,
 	],
 
 	[
-		"name" => "Movie Database",
-		"description" => "Created a movie database using HTML, CSS, PHP, and Word Press.",
+		"name" => "Sports Live Score Website",
+		"description" => "Shows live scores of games being played as well as past scores and future schedule of games.",
 		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
 		"case-study" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
-		"image" => "https://peprojects.dev/images/square.jpg",
+		"image" => "images/sports-site.svg",
 		"hot" => false,
+		"demo" => false,
+	],
+
+	[
+		"name" => "Student Grade Chart",
+		"description" => "User can create a student, enter student courses, add and update student grades.",
+		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"case-study" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"image" => "images/sgc.svg",
+		"hot" => false,
+		"demo" => false,
 	],
 ];
 
@@ -43,18 +59,21 @@ $bits = [
 		"name" => "Intergalactic Plant World",
 		"description" => "This was my first real client project. I was in charge with creating a website for a local small business plant shop.",
 		"project-link" => "https://peprojects.dev/alpha-1/alexvong/30day-responsive-layout-challenge/",
+		"has-project-site" => true,
 
 	],
 	[
 		"name" => "Exercises for Programmers",
 		"description" => "This is a collection of exercises from 'Exercises for Programmers'. I did these mostly in PHP",
 		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"has-project-site" => true,
 	
 	],
 	[
 		"name" => "Pet Adoption Site",
 		"description" => "This was my first website using responsive design. The goal was to make eveyr component scale on all device sizes.",
 		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"has-project-site" => true,
 	
 	],
 
@@ -62,55 +81,131 @@ $bits = [
 		"name" => "Super Responsive Layout Challenge",
 		"description" => "This project was focused on creating a super responsive layout that scaled across all sizes as well as build and reuse components.",
 		"project-link" =>"https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+		"has-project-site" => true,
 	
 	],
+
+	[
+		"name" => "Apple Site",
+		"description" => "A HTML and CSS Challenge I was given at Perpetual Education. GOal was to recreate a website as much as possible in an one hour time box",
+		"project-link" =>"https://codepen.io/Alexv0ng/pen/abpRozV",
+		"has-project-site" => false,
+	],
+
+	[
+		"name" => "Intro to CSS Box Model",
+		"description" => "An article written to give a basic and simple introduction to the CSS Box Model.",
+		"project-link" =>"https://codepen.io/Alexv0ng/pen/dyNdRgr",
+		"has-project-site" => false,
+	],
+
+	[
+		"name" => "CSS Positioning in Action",
+		"description" => "A quick demo showing all the different CSS positionings.",
+		"project-link" =>"https://codepen.io/Alexv0ng/pen/oNZxOoP",
+		"has-project-site" => false,
+	],
+
+	[
+		"name" => "Responsive Layout",
+		"description" => "A Template for a fully responsive website layout",
+		"project-link" =>"https://codepen.io/Alexv0ng/pen/gOmbOwb",
+		"has-project-site" => false,
+	],
+
+	[
+		"name" => "SVG Creations",
+		"description" => "This Codepen shows the code behing a few SVG images I created on Affinity Designer",
+		"project-link" =>"https://codepen.io/Alexv0ng/pen/LYxKMbO",
+		"has-project-site" => false,
+	],
+
 ];
 
 
-$codePens = [
-
-	[
-		"title" => "CSS Box Model",
-		"link" => "https://codepen.com",
-	],
-
-	[
-		"title" => "CSS Positions",
-		"link" => "https://codepen.com",
-	],
-
-	[
-		"title" => "SVG Creation",
-		"link" => "https://codepen.com",
-	],
-	[
-		"title" => "Responsive Layouts",
-		"link" => "https://codepen.com",
-	],
-];
 
 
 $latestBlog = [
 	[
-		"title" => "Responsive Layout Challenge Day 1",
-		"link" => "https://alexvong.substack.com/p/responsive-layout-challenge-day-1",
-		"date" => "October 26, 2021",
+		"title" => "The Code Behind an SVG",
+		"link" => "https://alexvong.substack.com/p/the-code-behind-svgs",
+		"description" => "Ever wonder what is under the hood of an SVG and what makes it so different than the other image formats?",
+		"demo" => false,
+		"front-page" => true,
 	],
+
+		[
+		"title" => "The Confidence I Needed to Pursue Web Development",
+		"link" => "https://community.codenewbie.org/alexv0ng/the-confidence-i-needed-to-pursue-web-development-2hh3",
+		"description" => "It is not easy trying to become a web developer when you have absolutely no experience. But finding the right guidance can help you accomplish your goals and set you down the right path.",
+		"demo" => false,
+		"front-page" => true,
+	],
+
+	[
+		"title" => "A Brief Intro to CSS Box Model",
+		"link" => "https://alexvong.substack.com/p/a-brief-intro-to-css-box-model",
+		"description" => "This is a thing called an html element. The whole 
+		thing starts with a box!",
+		"demo" => false,
+		"front-page" => true,
+	],
+
+	[
+		"title" => "Title of Article",
+		"link" => "https://alexvong.substack.com/p/the-code-behind-svgs",
+		"description" => "Here is a brief description or intro text that captivates the user and makes them want to click this link to read the full article",
+		"demo" => true,
+			"front-page" => false,
+	],
+	[
+		"title" => "Title of Another Awesome Article",
+		"link" => "https://alexvong.substack.com/p/the-code-behind-svgs",
+		"description" => "Here is a brief description or intro text that captivates the user and makes them want to click this link to read the full article",
+		"demo" => true,
+			"front-page" => false,
+	],
+	[
+		"title" => "Why Not One More Article But This Time With a Longer Title",
+		"link" => "https://alexvong.substack.com/p/the-code-behind-svgs",
+		"description" => "Here is a brief description or intro text that captivates the user and makes them want to click this link to read the full article",
+		"demo" => true,
+			"front-page" => false,
+	],
+
 	[
 		"title" => "30 Day Responsive Layout Challenge",
 		"link" => "https://alexvong.substack.com/p/30-day-responsive-layout-challenge",
-		"date" => "October 25, 2021",
+		"description" => "I decided to do a responsive layout challenge to expand my knowledge and skill of responsive design.",
+		"demo" => false,
+			"front-page" => false,
+	],
+
+	[
+		"title" => "Working with My First Client",
+		"link" => "https://alexvong.substack.com/p/working-with-my-first-client-an-intro",
+		"description" => 'A series documenting my experience working with my first "real" client. The excitment, the angst, the frustration, the joy, the fear, and the code. It is all there.',
+		"demo" => false,
+			"front-page" => false,
 	],
 	[
-		"title" => "The Condifence I Needed to Pursue Web Development",
-		"link" => "https://community.codenewbie.org/alexv0ng/the-confidence-i-needed-to-pursue-web-development-2hh3",
-		"date" => "August 24, 2021",
+		"title" => 'The New and "Improved" AOL',
+		"link" => "https://alexvong.substack.com/p/the-new-and-improved-aol",
+		"description" => "I built my first website ever and uploaded it to our class server using an FTP. Come read all about it!",
+		"demo" => false,
+			"front-page" => false,
 	],
+
 	[
-		"title" => "The Code Behind an SVG",
-		"link" => "https://alexvong.substack.com/p/the-code-behind-svgs",
-		"date" => "May 2, 2021",
-	]		
+		"title" => "AOL Wasn't the Internet?",
+		"link" => "https://alexvong.substack.com/p/a-brief-intro-to-css-box-model",
+		"description" => "Let's take a time machine back to the late 90s/early 2000s to explore what made American Online so amazing and how it set the foundation of what the internet is today!",
+		"demo" => false,
+			"front-page" => false,
+	],		
+		
+
+				
 
 ];
 
@@ -120,21 +215,25 @@ $favoriteBlog = [
 		"title" => "30 Day Responsive Layout Challenge",
 		"link" => "https://alexvong.substack.com/p/30-day-responsive-layout-challenge",
 		"date" => "October 25, 2021",
+		"hot" => true,
 	],
 	[
 		"title" => "The Condifence I Needed to Pursue Web Development",
 		"link" => "https://community.codenewbie.org/alexv0ng/the-confidence-i-needed-to-pursue-web-development-2hh3",
 		"date" => "August 24, 2021",
+		"hot" => true,
 	],
 	[
 		"title" => "Working with My First Client",
 		"link" => "https://alexvong.substack.com/p/working-with-my-first-client-an-intro",
 		"date" => "May 26, 2021",
+		"hot" => false,
 	],
 	[
 		"title" => "A Brief Intro to CSS Box Model",
 		"link" => "https://alexvong.substack.com/p/a-brief-intro-to-css-box-model",
 		"date" => "April 12, 2021",
+		"hot" => true,
 	],		
 
 ];
